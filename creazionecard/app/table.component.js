05dd60b1c13a769ac5eb66e9b0867308470ed820
Unknown <chiara.cardinali@edu.unito.it>
2018-06-14 10:40:58 +0200

    commento

--- creazionecard/app/table.component.js
+++ creazionecard/app/table.component.js
@@ -7,13 +7,13 @@ angular.module("table")
 			"card1", 
 			"card2", 
 			"card3",
-			"card4",
-			"card5",
-			"card6",
-			"card7",
-			"card8",
-			"card9",
-			"card10",
+			//"card4",
+			//"card5",
+			//"card6",
+			//"card7",
+			//"card8",
+			//"card9",
+			//"card10",
 			];
 
 		$scope.hours = [
